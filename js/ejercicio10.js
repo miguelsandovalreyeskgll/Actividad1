246e3206c7a1c9d384d9e1c5e32248985f005a0d
Sandoval Reyes Miguel <miguelsandovalreyeskgll@users.noreply.github.com>
2026-09-16 13:03:38 -0600

    Actualizar ejercicio 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,18 +1,16 @@
-const celsius = document.getElementById("celsius");
-const fahrenheit = document.getElementById("fahrenheit");
-const convertir = document.getElementById("convertir");
+function convertir() {
+    let celsius = document.getElementById("celsius").value;
 
-convertir.addEventListener("click", function () {
-    if (celsius.value === "") {
-        alert("Ingresa una temperatura en grados Celsius.");
+    if (celsius === "") {
+        alert("Ingresa una temperatura en grados Celsius");
         return;
     }
 
-    if (isNaN(celsius.value)) {
-        alert("Ingresa un valor numérico.");
+    if (isNaN(celsius)) {
+        alert("Ingresa un valor numérico");
         return;
     }
 
-    const resultado = (parseFloat(celsius.value) * 9 / 5) + 32;
-    fahrenheit.value = resultado + "°F";
-});
+    let fahrenheit = (parseFloat(celsius) * 9 / 5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit;
+}
